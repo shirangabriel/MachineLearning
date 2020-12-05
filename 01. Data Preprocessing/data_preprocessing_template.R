f82723667362d9203956f4c63bbf54721b635a99
Shiran Gabriel <shiran.gabriel@axiatadigitallabs.com>
2020-12-05 19:18:58 +0530

    Import data

--- 01. Data Preprocessing/data_preprocessing_template.R
+++ 01. Data Preprocessing/data_preprocessing_template.R
@@ -1,1 +1,4 @@
 # Data Preprocessing
+
+# Import the dataset
+dataset = read.csv('Data.csv')
--- 01. Data Preprocessing/data_preprocessing_template.py
+++ 01. Data Preprocessing/data_preprocessing_template.py
@@ -1,1 +1,1 @@
-# Data Preprocessing# Importing the librariesimport numpy as np import matplotlib.pyplot as pltimport pandas as pd# Import the dataset
+# Data Preprocessing# Importing the librariesimport numpy as np import matplotlib.pyplot as pltimport pandas as pd# Import the datasetdataset = pd.read_csv('Data.csv')x = dataset.iloc[:, :-1].valuesy = dataset.iloc[:, 3].values
